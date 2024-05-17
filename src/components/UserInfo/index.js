c9100301461b870a5b1c4a3e3b390b3c4e9e66e3
21a31a0458Aditya <21a31a0458@gmail.com>
2024-05-17 07:48:49 +0000

    blogList

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="UserInfo-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      className="profile-pic"
+      alt="profile"
+    />
+    <h1 className="User-Heading">Wade Warren</h1>
+    <p className="user-description">Software Developer at UK</p>
+  </div>
+)
+
+export default UserInfo
